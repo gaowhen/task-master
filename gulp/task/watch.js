5ffd54737dc75c2939d7ff2519890020c4b3d674
gaowhen <gaowhen.com@gmail.com>
2015-07-03 12:55:50 +0800

    new task: compile stylus

--- gulp/task/watch.js
+++ gulp/task/watch.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var config = require('config').gulp;
 var less = require('./less');
+var less = require('./stylus');
 var livereload = require('gulp-livereload');
 var watchify = require('./watchify');
 
@@ -10,5 +11,6 @@ gulp.task('watch', function () {
   });
 
   gulp.watch(config.src.css + '/**/*.less', ['less']).on('change', livereload.changed);
+  gulp.watch(config.src.css + '/**/*.styl', ['stylus']).on('change', livereload.changed);
   gulp.watch(config.src.js + '/**/*.js', ['watchify']).on('change', livereload.changed);
 });
