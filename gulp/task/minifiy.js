a6035a5e0847b14ac254ba828be6c781439eaf88
gaowhen <gaowhen.com@gmail.com>
2016-01-11 12:28:28 +0800

    replace minify with cssnano

--- gulp/task/minifiy.js
+++ gulp/task/minifiy.js
@@ -1,18 +1,18 @@
-var gulp = require('gulp');
-var config = require('config').gulp;
-var less = require('gulp-less');
-var minify = require('gulp-minify-css');
-var sourcemaps = require('gulp-sourcemaps');
+var gulp = require('gulp')
+var config = require('config').gulp
+var less = require('gulp-less')
+var cssnano = require('gulp-cssnano')
+var sourcemaps = require('gulp-sourcemaps')
 
 function compile() {
   gulp.src(config.src.css + '/**/*.less')
     .pipe(sourcemaps.init())
     .pipe(less())
-    .pipe(minify())
+    .pipe(cssnano())
     .pipe(sourcemaps.write('./map'))
-    .pipe(gulp.dest(config.dist.css));
+    .pipe(gulp.dest(config.dist.css))
 }
 
-gulp.task('minify', compile);
+gulp.task('minify', compile)
 
-module.exports = compile;
+module.exports = compile
