4e7baa29662d85f1f53e44780447080559f6730a
gaowhen <gaowhen.com@gmail.com>
2016-01-15 14:43:05 +0800

    feat: add show file size on processing

--- gulp/task/minifiy.js
+++ gulp/task/minifiy.js
@@ -3,12 +3,15 @@ var config = require('config').gulp
 var less = require('gulp-less')
 var cssnano = require('gulp-cssnano')
 var sourcemaps = require('gulp-sourcemaps')
+var size = require('gulp-filesize')
 
 function compile() {
   gulp.src(config.src.css + '/**/*.less')
     .pipe(sourcemaps.init())
     .pipe(less())
+    .pipe(size())
     .pipe(cssnano())
+    .pipe(size())
     .pipe(sourcemaps.write('./map'))
     .pipe(gulp.dest(config.dist.css))
 }
--- gulp/task/stylus.js
+++ gulp/task/stylus.js
@@ -4,11 +4,16 @@ var stylus = require('gulp-stylus');
 var jeet = require('jeet');
 var config = require('config').gulp;
 var sourcemaps = require('gulp-sourcemaps');
+var cssnano = require('gulp-cssnano')
+var size = require('gulp-filesize')
 
 gulp.task('stylus', function () {
   return gulp.src(config.src.css + '/**/*.styl')
     .pipe(sourcemaps.init())
+    .pipe(size())
     .pipe(stylus({use: [nib(), jeet()], 'include css': true}))
+    .pipe(cssnano())
+    .pipe(size())
     .pipe(sourcemaps.write('./map'))
     .pipe(gulp.dest(config.dist.css));
 });
